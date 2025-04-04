2b15027268e8586001deb4c5039c3704e453d5e3
Shubham-Haldkar <shubham.haldkar@gmail.com>
2025-04-04 18:59:36 +0530

    for each loop

---
+++ src/loops/ForEachLoop.java
@@ -0,0 +1,11 @@
+package loops;
+
+public class ForEachLoop {
+    public static void main(String[] args) {
+        String names[] = {"a","b","c"} ;
+
+        for (String s : names) {
+            System.out.println(s);
+        }
+    }
+}
