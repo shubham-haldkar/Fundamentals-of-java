11f1a0c51e2b5555beaa00febc4c7df44ba99cfe
Shubham-Haldkar <shubham.haldkar@gmail.com>
2025-04-04 18:50:24 +0530

    If statement

---
+++ src/conditions/IfStatement.java
@@ -0,0 +1,13 @@
+package conditions;
+
+public class IfStatement {
+    public static void main(String[] args) {
+        boolean isAuth = true;
+
+        if (isAuth){
+            System.out.println("Logged in");
+        }else{
+            System.out.println("Not logged in");
+        }
+    }
+}
